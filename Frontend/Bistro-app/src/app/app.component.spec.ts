4e8aec5a304c44b516fd3a4e4ef97307c4b35306
filipsupolik25@gmail.com <14Filip14>
2021-10-05 11:21:50 +0200

    Starting frontend

--- Frontend/Bistro-app/src/app/app.component.spec.ts
+++ Frontend/Bistro-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Restaurant-app'`, () => {
+  it(`should have as title 'Bistro-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Restaurant-app');
+    expect(app.title).toEqual('Bistro-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Restaurant-app app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Bistro-app app is running!');
   });
 });
