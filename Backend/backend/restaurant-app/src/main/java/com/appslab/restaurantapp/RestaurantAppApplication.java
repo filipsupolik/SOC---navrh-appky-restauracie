9c81610ca742ed9520ef0e8f815efc167bce3c06
openlab <openlab@z00p1-17.SPSKNM>
2021-10-05 10:51:25 +0200

    Adding spring boot files

---
+++ Backend/backend/restaurant-app/src/main/java/com/appslab/restaurantapp/RestaurantAppApplication.java
@@ -0,0 +1,14 @@
+package com.appslab.restaurantapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantAppApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
