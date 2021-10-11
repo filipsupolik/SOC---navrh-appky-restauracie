aaad3102cba1288f66594c2585b856f3e933d832
Martin3 <strbanik.jun@gmail.com>
2021-10-11 14:27:04 +0200

    Added admin user

--- Backend/backend/restaurant-app/src/main/java/com/appslab/restaurantapp/DefaultInitialization.java
+++ Backend/backend/restaurant-app/src/main/java/com/appslab/restaurantapp/DefaultInitialization.java
@@ -1,6 +1,7 @@
 package com.appslab.restaurantapp;
 
 import com.appslab.restaurantapp.user.User;
+import com.appslab.restaurantapp.user.UserRepository;
 import com.appslab.restaurantapp.user.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -9,14 +10,21 @@ import org.springframework.stereotype.Component;
 public class DefaultInitialization implements CommandLineRunner {
 
     UserService userService;
+    UserRepository userRepository;
 
-    public DefaultInitialization(UserService userService) {
+    public DefaultInitialization(UserService userService, UserRepository userRepository) {
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        User user = new User("Martin", "password");
-        userService.createUser(user);
+
+        if (userRepository.findByUsername("admin").isPresent()==false){
+            User user = new User("admin", "admin");
+            userService.createUser(user);
+        }
+
+
     }
 }
--- Backend/backend/restaurant-app/src/main/java/com/appslab/restaurantapp/user/UserRepository.java
+++ Backend/backend/restaurant-app/src/main/java/com/appslab/restaurantapp/user/UserRepository.java
@@ -2,6 +2,9 @@ package com.appslab.restaurantapp.user;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 
 }
