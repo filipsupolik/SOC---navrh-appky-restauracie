8df19661baad572e5f1c96d42404303ee5701b99
Filip <filipsupolik25@gmail.com>
2021-10-21 16:38:52 +0200

    some changes

--- Frontend/Bistro-app/src/app/nav-bar/nav-bar.component.ts
+++ Frontend/Bistro-app/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from 'src/Services/token-storage.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,12 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
+  private roles = string[];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username: string | undefined;
 
-  constructor() { }
-
-  
-
+  constructor (private tokenStorageService: TokenStorageService) { }
   ngOnInit(): void {
-  }
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
 
+    if(this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_Moderator');
+
+      this.username = user.displayName;
+    }
+  } 
+
+  logout(): void {
+    this.tokenStorageService.signOut();
+    window.location.reload();
+  }
 }
