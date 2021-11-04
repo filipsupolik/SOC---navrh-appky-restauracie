67c6687e854201761cb4469c7245d20b1c7eae44
openlab <openlab@z00p1-17.SPSKNM>
2021-11-04 10:07:55 +0100

    Working on fixing cors issue

--- Backend/backend/restaurant-app/src/main/java/com/appslab/restaurantapp/security/SpringSecurityConfig.java
+++ Backend/backend/restaurant-app/src/main/java/com/appslab/restaurantapp/security/SpringSecurityConfig.java
@@ -1,19 +1,23 @@
 package com.appslab.restaurantapp.security;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@AllArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().
-                disable()
+        http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS, "/**")
                 .permitAll()
@@ -21,5 +25,21 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("*")
+                        .allowCredentials(true);
+            }
+
+        };
     }
 }
