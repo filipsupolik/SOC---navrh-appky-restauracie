cf9b42f49c74bc744db5e1ee0d375424461a8c89
filipsupolik25@gmail.com <14Filip14>
2021-11-25 10:52:05 +0100

    started with restaurant page design

--- Frontend/Bistro-app/src/app/app-routing.module.ts
+++ Frontend/Bistro-app/src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
-import { SearchRestaurantComponent } from './searching_restaurants/search-restaurant/search-restaurant.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AllRestaurantsComponent } from './all-restaurants/all-restaurants.component';
+import { RestaurantPageComponent } from './restaurant-page/restaurant-page.component';
 
 const routes: Routes = [
   {path: '', component: MainPageComponent},
@@ -17,6 +16,7 @@ const routes: Routes = [
   {path: 'main-page', component: MainPageComponent},
   {path: 'profile', component: ProfileComponent},
   {path: 'restaurants', component: AllRestaurantsComponent},
+  {path: 'restaurantpage', component: RestaurantPageComponent},
 ];
 
 @NgModule({
