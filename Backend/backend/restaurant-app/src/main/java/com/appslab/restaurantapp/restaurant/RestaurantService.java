9761f296cec78ff0df86cd811b525e9d8b9b1680
Martin3 <strbanik.jun@gmail.com>
2022-01-24 18:18:04 +0100

    Added adminId for restaurants

--- Backend/backend/restaurant-app/src/main/java/com/appslab/restaurantapp/restaurant/RestaurantService.java
+++ Backend/backend/restaurant-app/src/main/java/com/appslab/restaurantapp/restaurant/RestaurantService.java
@@ -1,12 +1,15 @@
 package com.appslab.restaurantapp.restaurant;
 
 
+import com.appslab.restaurantapp.exception.GenericException;
+
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 
 public interface RestaurantService {
 
-    void createRestaurant(Restaurant restaurant);
+    void createRestaurant(Restaurant restaurant, Principal principal)throws GenericException;
     List<Restaurant> getRestaurantsByCategory(String category);
     Optional<Restaurant> getRestaurantById(long id);
     Restaurant getRestaurantInfo(Long restaurantName);
