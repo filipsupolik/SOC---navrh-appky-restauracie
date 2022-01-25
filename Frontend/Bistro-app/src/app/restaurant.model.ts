fc8fb41b9e5fc6e5f3b4a9b0e56ea2c8721be3d8
filipsupolik25@gmail.com <14Filip14>
2022-01-25 11:42:10 +0100

    Updating restaurant model

--- Frontend/Bistro-app/src/app/restaurant.model.ts
+++ Frontend/Bistro-app/src/app/restaurant.model.ts
@@ -3,5 +3,9 @@ import { Food } from "./food.model";
 export interface Restaurant {
     id: number;
     restaurantName: string;
+    description: string;
+    deliveryTimeMinutes: number;
+    openingTime: string;
+    address: string;
     food: Food[];
 }
