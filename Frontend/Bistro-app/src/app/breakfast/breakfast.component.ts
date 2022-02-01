b334f9e574c05353d14a491b8027cbc40ef9eaff
filipsupolik25@gmail.com <14Filip14>
2022-02-01 12:01:30 +0100

    Working app again, continue in conecting

--- Frontend/Bistro-app/src/app/breakfast/breakfast.component.ts
+++ Frontend/Bistro-app/src/app/breakfast/breakfast.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
+import { Restaurant } from '../restaurant.model';
+import { RestaurantService } from '../_services/restaurant.service';
 
 @Component({
   selector: 'app-breakfast',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreakfastComponent implements OnInit {
 
-  constructor() { }
+  restaurants$: Observable <Restaurant[]> = EMPTY;
+
+  constructor(private restaurantService: RestaurantService) { }
 
   ngOnInit(): void {
+    this.restaurants$ = this.restaurantService.getRestaurantsByCategory('Breakfast');
   }
 
 }
