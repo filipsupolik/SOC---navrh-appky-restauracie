44d2b2784b9b7ec1b79a4f6de349de060c5ce932
Martin3 <strbanik.jun@gmail.com>
2022-03-31 17:31:19 +0200

    Removed old security (securityOld package was new security! Now it is named security!)

--- Backend/backend/restaurant-app/src/main/java/com/appslab/restaurantapp/category/CategoryRepository.java
+++ Backend/backend/restaurant-app/src/main/java/com/appslab/restaurantapp/category/CategoryRepository.java
@@ -2,7 +2,6 @@ package com.appslab.restaurantapp.category;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
